1e527ea25bb73d0cd2189d2d3321599878769825
Alfa Sunny <alfasunny93@gmail.com>
2022-05-20 23:17:56 +0600

    Initial commit

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use Illuminate\Support\Facades\Auth;
+
+class HomeController extends Controller
+{
+    public function index(){
+        return view('home');
+    } 
+    
+    
+    public function redirects(){
+        $userType = Auth::user()->usertype;
+
+        if($userType == '1'){
+            return view('admin.admin');
+        }
+
+        else{
+            return view('home');
+        }
+    }
+}
